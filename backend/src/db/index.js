b2f5ff2be9d650971b955460daacfff78fb31c50
harsh-coder-desgin <harshpatel51057@gmail.com>
2026-06-06 11:48:05 +0530

    mogodb setup

---
+++ backend/src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import dotenv from "dotenv"
-dotenv.config()
+import connectDB from "./db/index.js";
+dotenv.config({
+   path: "./.env",
+});
 
 import app from "./app.js"
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-   console.log(`Server running on ${PORT}`)
-})
+connectDB()
+   .then(() => {
+      app.listen(process.env.PORT || 5000, () => {
+         console.log(`Server running on port ${process.env.PORT}`);         
+      });
+   })
+   .catch((err) => {
+      console.log("MongoDB Connection Failed:", err);
+   });
